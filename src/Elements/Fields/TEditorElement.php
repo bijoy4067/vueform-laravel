a3db362cb7bfdf21ef9a906b0b0b9b43ba7d6bfc
bijoy4067 <52704882+bijoy4067@users.noreply.github.com>
2025-12-31 01:46:40 +0600

    r

---
+++ src/Elements/Fields/TEditorElement.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace LaravelVueForm\Elements\Fields;
+
+use LaravelVueForm\Builder\FormSchemaBuilder;
+
+class TEditorElement extends FormSchemaBuilder
+{
+    protected static array $allowedAttributes = [
+        'accept' => 'array', // default: 'null'
+        'acceptMimes' => 'array', // default: 'null'
+        'addClass' => 'array|object|string|function', // default: 'null'
+        'addClasses' => 'object|function', // default: '{}'
+        'after' => 'object|string|number', // default: 'null'
+        'before' => 'object|string|number', // default: 'null'
+        'between' => 'object|string|number', // default: 'null'
+        'columns' => 'object|string|number', // default: 'null'
+        'conditions' => 'array', // default: '[]'
+        'debounce' => 'number', // default: 'null'
+        'default' => 'object|string|number', // default: 'null'
+        'description' => 'string|object', // default: 'null'
+        'disabled' => 'boolean|function|array|object', // default: 'false'
+        'displayErrors' => 'boolean', // default: 'true'
+        'endpoint' => 'string|function|promise', // default: 'config.endpoints.attachment.url'
+        'fieldName' => 'string|object', // default: 'name|label'
+        'formatData' => 'function', // default: 'null'
+        'formatLoad' => 'function', // default: 'null'
+        'hideTools' => 'array', // default: '[]'
+        'id' => 'string', // default: 'null'
+        'info' => 'string|object', // default: 'null'
+        'infoPosition' => 'string', // default: 'right'
+        'inline' => 'boolean', // default: 'false'
+        'label' => 'string|object|function', // default: 'null'
+        'messages' => 'object', // default: '{}'
+        'method' => 'string', // default: 'config.endpoints.attachment.method'
+        'name' => 'string|number', // default: 'undefined'
+        'overrideClass' => 'array|object|string|function', // default: 'null'
+        'overrideClasses' => 'object|function', // default: '{}'
+        'placeholder' => 'string|object', // default: 'null'
+        'presets' => 'array', // default: '[]'
+        'removeClass' => 'array|object|function', // default: 'null'
+        'removeClasses' => 'object|function', // default: '{}'
+        'replaceClass' => 'object|function', // default: 'null'
+        'replaceClasses' => 'object|function', // default: '{}'
+        'rules' => 'array|string|object', // default: 'null'
+        'size' => 'string', // default: 'undefined'
+        'slots' => 'object', // default: '{}'
+        'submit' => 'boolean', // default: 'true'
+        'templates' => 'object', // default: '{}'
+        'view' => 'string', // default: 'undefined'
+        'views' => 'object', // default: '{}'
+    ];
+
+    protected static array $allowedProperties = [
+        'aria' => 'object',
+        'available' => 'boolean',
+        'busy' => 'boolean',
+        'classes' => 'object',
+        'container' => 'HTMLElement',
+        'data' => 'object',
+        'dataPath' => 'string',
+        'debouncing' => 'boolean',
+        'dirty' => 'boolean',
+        'el_' => 'VueformElement',
+        'empty' => 'boolean',
+        'error' => 'string',
+        'errors' => 'array',
+        'fieldId' => 'string',
+        'focused' => 'boolean',
+        'form_' => 'Vueform',
+        'hasLabel' => 'boolean',
+        'hidden' => 'boolean',
+        'input' => 'HTMLElement',
+        'invalid' => 'boolean',
+        'isDanger' => 'boolean',
+        'isDefault' => 'boolean',
+        'isDisabled' => 'boolean',
+        'isFilled' => 'boolean',
+        'isRequired' => 'boolean',
+        'isSuccess' => 'boolean',
+        'language' => 'string',
+        'languages' => 'array',
+        'messageBag' => 'MessageBag',
+        'model' => 'any',
+        'mounted' => 'boolean',
+        'parent' => 'VNode',
+        'path' => 'string',
+        'pending' => 'boolean',
+        'placeholder' => 'string',
+        'requestData' => 'object',
+        'size' => 'string',
+        'template' => 'object',
+        'theme' => 'object',
+        'useCustomFilled' => 'boolean',
+        'validated' => 'boolean',
+        'value' => 'any',
+        'view' => 'string',
+        'visible' => 'boolean',
+    ];
+
+    protected static array $allowedEventAttributes = [
+        'reset',
+        'clear',
+        'change',
+        'blur',
+        'alert',
+        'error',
+        'before-create',
+        'created',
+        'before-mount',
+        'mounted',
+        'before-update',
+        'updated',
+        'before-unmount',
+        'unmounted',
+    ];
+
+    protected static array $allowedSlotAttributes = [
+        'label' => [],
+        'info' => [],
+        'required' => [],
+        'description' => [],
+        'before' => [],
+        'between' => [],
+        'after' => [],
+    ];
+}
---
+++ src/Elements/Fields/TTextareaElement.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace LaravelVueForm\Elements\Fields;
+
+use LaravelVueForm\Builder\FormSchemaBuilder;
+
+class TTextareaElement extends FormSchemaBuilder
+{
+    protected static array $allowedAttributes = [
+        'addClass' => 'array|object|string|function', // default: 'null'
+        'addClasses' => 'object|function', // default: '{}'
+        'addons' => 'object', // default: '{}'
+        'after' => 'object|string|number', // default: 'null'
+        'attrs' => 'object', // default: '{}'
+        'autogrow' => 'boolean', // default: 'true'
+        'before' => 'object|string|number', // default: 'null'
+        'between' => 'object|string|number', // default: 'null'
+        'columns' => 'object|string|number', // default: 'null'
+        'conditions' => 'array', // default: '[]'
+        'debounce' => 'number', // default: 'null'
+        'default' => 'object|string|number', // default: 'null'
+        'description' => 'string|object', // default: 'null'
+        'disabled' => 'boolean|function|array|object', // default: 'false'
+        'displayErrors' => 'boolean', // default: 'true'
+        'fieldName' => 'string|object', // default: 'name|label'
+        'floating' => 'string|boolean|object', // default: 'null'
+        'formatData' => 'function', // default: 'null'
+        'formatLoad' => 'function', // default: 'null'
+        'id' => 'string', // default: 'null'
+        'info' => 'string|object', // default: 'null'
+        'infoPosition' => 'string', // default: 'right'
+        'inline' => 'boolean', // default: 'false'
+        'label' => 'string|object|function', // default: 'null'
+        'messages' => 'object', // default: '{}'
+        'name' => 'string|number', // default: 'undefined'
+        'overrideClass' => 'array|object|string|function', // default: 'null'
+        'overrideClasses' => 'object|function', // default: '{}'
+        'placeholder' => 'string|object', // default: 'null'
+        'presets' => 'array', // default: '[]'
+        'readonly' => 'boolean|function|array|object', // default: 'false'
+        'removeClass' => 'array|object|function', // default: 'null'
+        'removeClasses' => 'object|function', // default: '{}'
+        'replaceClass' => 'object|function', // default: 'null'
+        'replaceClasses' => 'object|function', // default: '{}'
+        'rows' => 'number', // default: '3'
+        'rules' => 'array|string|object', // default: 'null'
+        'size' => 'string', // default: 'undefined'
+        'slots' => 'object', // default: '{}'
+        'submit' => 'boolean', // default: 'true'
+        'templates' => 'object', // default: '{}'
+        'view' => 'string', // default: 'undefined'
+        'views' => 'object', // default: '{}'
+    ];
+
+    protected static array $allowedProperties = [
+        'aria' => 'object',
+        'available' => 'boolean',
+        'busy' => 'boolean',
+        'classes' => 'object',
+        'container' => 'HTMLElement',
+        'dataPath' => 'string',
+        'debouncing' => 'boolean',
+        'dirty' => 'boolean',
+        'el_' => 'VueformElement',
+        'empty' => 'boolean',
+        'error' => 'string',
+        'errors' => 'array',
+        'fieldId' => 'string',
+        'focused' => 'boolean',
+        'form_' => 'Vueform',
+        'hasFloating' => 'boolean',
+        'hasLabel' => 'boolean',
+        'hidden' => 'boolean',
+        'input' => 'HTMLElement',
+        'invalid' => 'boolean',
+        'isDanger' => 'boolean',
+        'isDefault' => 'boolean',
+        'isDisabled' => 'boolean',
+        'isFilled' => 'boolean',
+        'isReadonly' => 'boolean',
+        'isRequired' => 'boolean',
+        'isSuccess' => 'boolean',
+        'language' => 'string',
+        'languages' => 'array',
+        'messageBag' => 'MessageBag',
+        'model' => 'any',
+        'mounted' => 'boolean',
+        'parent' => 'VNode',
+        'path' => 'string',
+        'pending' => 'boolean',
+        'placeholder' => 'string',
+        'size' => 'string',
+        'template' => 'object',
+        'theme' => 'object',
+        'useCustomFilled' => 'boolean',
+        'validated' => 'boolean',
+        'value' => 'any',
+        'view' => 'string',
+        'visible' => 'boolean',
+    ];
+
+    protected static array $allowedEventAttributes = [
+        'reset',
+        'clear',
+        'change',
+        'blur',
+        'focus',
+        'keydown',
+        'keyup',
+        'keypress',
+        'before-create',
+        'created',
+        'before-mount',
+        'mounted',
+        'before-update',
+        'updated',
+        'before-unmount',
+        'unmounted',
+    ];
+
+    protected static array $allowedSlotAttributes = [
+        'label' => [],
+        'info' => [],
+        'required' => [],
+        'description' => [],
+        'before' => [],
+        'between' => [],
+        'after' => [],
+        'addon-before' => [],
+        'addon-after' => [],
+    ];
+}
